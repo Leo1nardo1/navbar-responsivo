4270f1eda8ea0239c53da97cc7bc6ba4d51684d0
Leo1nardo1 <leonardo.carvalho.ferreira@protonmail.com>
2024-08-13 01:57:02 -0300

    Js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,7 @@
-// javascript/javascript.js
-
-document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.querySelector('.burger');
-  const mobileMenu = document.querySelector('.mobile-menu');
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.mobile-menu');
 
   burger.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
   });
-});
+
